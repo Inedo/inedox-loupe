20ec1a47738d883136e2d23d44bcfba76fb87838
InedoJohn <jrasch@inedo.com>
2019-01-16 17:59:56 -0500

    Initial commit

---
+++ Loupe/InedoExtension/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Inedo.Extensibility;
+
+[assembly: AssemblyTitle("Loupe")]
+[assembly: AssemblyDescription("Contains operations to manipulate issues and change release types in Loupe.")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyProduct("BuildMaster")]
+[assembly: AssemblyCopyright("Copyright © 2008 - 2019")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(false)]
+
+[assembly: AppliesTo(InedoProduct.BuildMaster)]
